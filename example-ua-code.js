22d739406bf4f92d394cc0ed891cbe2f45e08973
Daniel Huigens <d.huigens@gmail.com>
2017-04-23 21:13:27 +0200

    Switch to the same format of hashes as used in Subresource Integrity

--- example-ua-code.js
+++ example-ua-code.js
@@ -1,59 +1,27 @@
 function onResponseBody(request, response) {
-	if (request.status >= 400 && request.destination === "document") {
+	if (response.status >= 400 && request.destination !== "document") { // Defined by the Fetch spec (https://fetch.spec.whatwg.org/)
 		return; // let response through
 	}
 	let pairs = request.associatedCertificate.extensions.httpsContentSignatures; // ID: 2.5.29.66
 	if (!pairs) {
 		return; // let response through
 	}
-	let hashSpecified = false;
-	let hashMatched = false;
 	for (let pair of pairs) {
 		if(pair.path === request.url || pair.path === request.path) {
-			hashSpecified = true;
-			if(pair.hash === response.hash) {
-				hashMatched = true;
+			if (response.status !== 200 && response.status !== 302 && response.status < 400) {
+				request.abort();
+				// show security error in browsing context
 			}
+			if (!match(pair.hash, response.body)) { // Defined by Subresource Integrity (https://www.w3.org/TR/SRI/)
+				request.abort();
+				// show security error in browsing context
+			}
+			break;
 		}
 	}
-	if (hashSpecified && (!hashMatched || (request.status !== 200 && request.status !== 302 && request.status < 400))) {
-		request.abort();
-		// show security error in browsing context
-	}
+	return; // let response through
 }
-
 
 // Note: the order of pairs is not defined, so the user agent may
-// optimize the above code by, for example, collecting all the hashes
-// for each path in arrays and put those in a hash table, and look up
-// the hashes in that table by path and by url for each request, for
-// example as follows:
-
-function onCertificateReceived(certificate) {
-	let pairs = certificate.extensions.httpsContentSignatures; // ID: 2.5.29.66
-	if (pairs) {
-		let table = {};
-		for (let pair of pairs) {
-			if(!table[pair.path]) {
-				table[pair.path] = [];
-			}
-			table[pair.path].push(pair.hash);
-		}
-		certificate.httpsContentSignaturesTable = table;
-	}
-}
-
-function onResponseBodyReceived(request, response) {
-	if (request.status >= 400 && request.destination === "document") {
-		return; // let response through
-	}
-	let table = request.associatedCertificate.httpsContentSignaturesTable;
-	if (!table) {
-		return; // let response through
-	}
-	let hashes = table[request.url] || table[request.path];
-	if (hashes && (!hashes.contains(response.hash) || (request.status !== 200 && request.status !== 302 && request.status < 400))) {
-		request.abort();
-		// show security error in browsing context
-	}
-}
+// optimize the above code to, for example, a pair of hash table
+// lookups.
